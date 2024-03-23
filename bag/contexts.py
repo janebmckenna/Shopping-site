16dea3de10c2d6e2c2d10b906ec9ecaea4fdbeea
jane mckenna <janemckenna@hotmail.com>
2024-03-23 18:22:39 +0000

    Linting

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
+
 def bag_contents(request):
 
     bag_items = []
@@ -16,25 +17,25 @@ def bag_contents(request):
             total += item_data * product.price
             product_count += item_data
             bag_items.append({
-                'item_id' : item_id,
-                'quantity' : item_data,
-                'product' : product,
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
             })
         else:
             for size, quantity in item_data['items_by_size'].items():
                 total += quantity * product.price
                 product_count += quantity
                 bag_items.append({
-                    'item_id' : item_id,
-                    'quantity' : quantity,
-                    'product' : product,
-                    'size' : size,
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
                 })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
-    else: 
+    else:
         delivery = 0
         free_delivery_delta = 0
 
@@ -42,12 +43,12 @@ def bag_contents(request):
 
     context = {
         'bag_items': bag_items,
-        'total' : total,
-        'product_count' : product_count,
-        'delivery' : delivery,
-        'free_delivery_delta' : free_delivery_delta,
-        'free_delivery_threshold' : settings.FREE_DELIVERY_THRESHOLD,
-        'grand_total' : grand_total,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
     }
 
     return context
--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render,redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404)
 from django.contrib import messages
 
 from products.models import Product
+
 
 # Create your views here.
 def view_bag(request):
@@ -27,7 +29,8 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
-                messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
+                messages.success(
+                    request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
             else:
                 bag[item_id]['items_by_size'][size] = quantity
                 messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
